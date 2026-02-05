c237de6d38d7a4dbd9d0ff6a7ce5afd3317f013f
Francois-Coleongco <christien.coleongco@gmail.com>
2026-02-04 20:37:58 -0500

    updated isograb to use Ubuntu 24

--- iso_grab.sh
+++ iso_grab.sh
@@ -1,4 +1,4 @@
 #!/bin/env bash
 
-wget https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img
-qemu-img create -f qcow2 -o backing_file=jammy-server-cloudimg-amd64.img,backing_fmt=qcow2 bean.qcow2 10G
+wget https://cloud-images.ubuntu.com/noble/current/noble-server-cloudimg-amd64.img
+qemu-img create -f qcow2 -o backing_file=noble-server-cloudimg-amd64.img,backing_fmt=qcow2 bean.qcow2 96G
